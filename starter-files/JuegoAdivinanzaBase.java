5749953ffe25c3bcacff70e9d608368d3c55fcf8
genixmvp <genix.mvp@gmail.com>
2026-06-19 16:24:41 -0500

    feature: add juego adivinanza base

---
+++ starter-files/JuegoAdivinanzaBase.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class JuegoAdivinanzaBase {
+    // Aquí va el atributo oculto para el número secreto
+
+    public void iniciar() {
+        Scanner lector = new Scanner(System.in);
+        System.out.println("¡Bienvenido al Juego de Adivinanzas!");
+
+        // Aquí debes crear tu bucle while
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Instancia tu juego aquí
+    }
+}
